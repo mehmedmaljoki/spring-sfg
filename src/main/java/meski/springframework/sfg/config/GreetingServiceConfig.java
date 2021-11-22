95e9a19f7faf38eb0816768fd912d4e76dec5d98
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2021-11-22 22:47:20 +0100

    springboot profiles application.properties

--- src/main/java/meski/springframework/sfg/config/GreetingServiceConfig.java
+++ src/main/java/meski/springframework/sfg/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import meski.springframework.sfg.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfg-config.xml")
 @Configuration
 public class GreetingServiceConfig {
